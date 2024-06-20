1b3c19c606b9f81eeb99c493cb3f4e18b5cabcec
Mr. KK <163104299+Kiran-Kumar-981@users.noreply.github.com>
2024-06-20 20:55:05 +0530

    Update validatingInput.go

--- helper/validatingInput.go
+++ helper/validatingInput.go
@@ -1,7 +1,7 @@
 package helper
 
 import "strings"
-
+//ValidateUserInput validate the user input and validate tickets how many are remaining and how many he can book 
 func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
