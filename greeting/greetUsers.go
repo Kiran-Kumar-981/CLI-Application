523f894ab0377e2604964b94ff5c0b1eb4132ee9
Mr. KK <163104299+Kiran-Kumar-981@users.noreply.github.com>
2024-06-20 20:52:57 +0530

    Update greetUsers.go

--- greeting/greetUsers.go
+++ greeting/greetUsers.go
@@ -5,7 +5,7 @@ import (
 )
 
 const conferenceTickets uint = 250
-
+//GreetUsers is trigered to greet the customer and show how many available tickets he can book and redirects to booking package for user input
 func GreetUsers(comedian, conferenceName string, remainingTickets, count uint) {
 	fmt.Printf("%v welcomes you to the show\n", comedian)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
